2fe06f03d03f1f82829c8aa8a1aeece2ee158aff
alexgarcia98 <haloalex68@gmail.com>
2019-01-19 15:33:17 -0800

    testing location

--- mobell/app/src/main/java/com/example/mobell/MainActivity.java
+++ mobell/app/src/main/java/com/example/mobell/MainActivity.java
@@ -1,74 +1,105 @@
 package com.example.mobell;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+
 import java.net.*;
 import java.io.*;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.mobell.MESSAGE";
+    private FusedLocationProviderClient mFusedLocationClient;
+    private boolean mLocationPermissionGranted;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
     }
 
-    // START
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
 
-   public String getStore(String coordinate) {
-      String req =
-         "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?" +
-         "input=store&" +
-         "inputtype=textquery&" +
-         "fields=formatted_address,name&" +
-         "locationbias=circle:20@" + coordinate + "&" +
-         "key=" + YOUR_API_KEY;
-         URL url = new URL(req);
-         HttpURLConnection con = (HttpURLConnection)url.openConnection();
-         try {
-            con.setRequestMethod("GET");
-         }
-         catch (ProtocolException e) {
-            System.out.println("ProtocolException: " + e);
-         }
-         int status = con.getResponseCode();
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+    }
 
-         System.out.println("status: " + status);
-
-         BufferedReader in = new BufferedReader(
-           new InputStreamReader(con.getInputStream()));
-         String inputLine;
-         StringBuffer content = new StringBuffer();
-         while ((inputLine = in.readLine()) != null) {
-            content.append(inputLine);
-         }
-         in.close();
-         con.disconnect();
-
-         System.out.println(con.getResponseMessage());
-         return content.toString();
-      }
-      catch (Exception e) {
-         return "Exception: " + e;
-      }
-   }
-
-   // END
+    public Location getLocation() {
+        Location l = null;
+        getLocationPermission();
+        try {
+            Task<Location> res = mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                // Logic to handle location object
+                            }
+                        }
+                    });
+            if(res.isSuccessful() && res.getResult() != null) {
+                l = res.getResult();
+            }
+        }
+        catch (SecurityException e) {
+            System.out.println("Locations are off");
+        }
+        return l;
+    }
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
-        String coordinate = "35.280895799999996,-120.66354899999999";
-        String resp = getStore(coordinate);
         Intent intent = new Intent(this, DisplayMessageActivity.class);
+        Location l = getLocation();
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
-
-        intent.putExtra(EXTRA_MESSAGE, resp);
+        intent.putExtra(EXTRA_MESSAGE, l.getLatitude() + "," + l.getLongitude());
         startActivity(intent);
     }
 }
