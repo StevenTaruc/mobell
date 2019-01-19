ec5f950c5c96f2d9ec056bf9c0c822a4dd40cfa1
alexgarcia98 <haloalex68@gmail.com>
2019-01-19 03:13:01 -0800

    testing maps

--- mobell/app/src/main/java/com/example/mobell/MainActivity.java
+++ mobell/app/src/main/java/com/example/mobell/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import java.net.*;
+import java.io.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,13 +18,58 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    // START
+
+   public String getStore(String coordinate) {
+      String req =
+         "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?" +
+         "input=store&" +
+         "inputtype=textquery&" +
+         "fields=formatted_address,name&" +
+         "locationbias=circle:20@" + coordinate + "&" +
+         "key=" + YOUR_API_KEY;
+      try {
+         URL url = new URL(req);
+         HttpURLConnection con = (HttpURLConnection)url.openConnection();
+         try {
+            con.setRequestMethod("GET");
+         }
+         catch (ProtocolException e) {
+            System.out.println("ProtocolException: " + e);
+         }
+         int status = con.getResponseCode();
+
+         System.out.println("status: " + status);
+
+         BufferedReader in = new BufferedReader(
+           new InputStreamReader(con.getInputStream()));
+         String inputLine;
+         StringBuffer content = new StringBuffer();
+         while ((inputLine = in.readLine()) != null) {
+             content.append(inputLine);
+         }
+         in.close();
+         con.disconnect();
+
+         System.out.println(con.getResponseMessage());
+         return content;
+      }
+      catch (Exception e) {
+         return "Exception: " + e;
+      }
+   }
+
+   // END
+
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
+        String coordinate = "35.280895799999996,-120.66354899999999";
+        String resp = getStore(coordinate);
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
 
-        intent.putExtra(EXTRA_MESSAGE, message);
+        intent.putExtra(EXTRA_MESSAGE, resp);
         startActivity(intent);
     }
 }
